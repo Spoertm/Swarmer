28c843264acf045e10448ae42451aa90f6e91d86
Spoertm <47951676+Spoertm@users.noreply.github.com>
2021-06-24 18:37:06 +0200

    Update Config.cs

--- Models/Config.cs
+++ Models/Config.cs
@@ -2,12 +2,12 @@
 {
 	public class Config
 	{
-		public string BotToken { get; set; }
-		public string ClientId { get; set; }
-		public string AccessToken { get; set; }
-		public ulong[] NotifChannelIds { get; set; }
-		public string Prefix { get; set; }
-		public string ReactionEmote { get; set; }
+		public string BotToken { get; set; } = null!;
+		public string ClientId { get; set; } = null!;
+		public string AccessToken { get; set; } = null!;
+		public ulong[] NotifChannelIds { get; set; } = null!;
+		public string Prefix { get; set; } = null!;
+		public string ReactionEmote { get; set; } = null!;
 		public ulong SwarmerInfoChannelId { get; set; }
 		public ulong SwarmerActiveStreamsChannelId { get; set; }
 	}
