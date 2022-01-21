a2a653b3a7b567412d3a7fb3bc65023e73893e9f
Spoertm <spoertm@gmail.com>
2022-01-21 15:36:58 +0100

    Implement stream buffer
    So that streams can't be posted twice or more if they're in the buffer

--- Services/DdStreamsPostingService.cs
+++ Services/DdStreamsPostingService.cs
@@ -1,6 +1,5 @@
 ﻿global using Stream = TwitchLib.Api.Helix.Models.Streams.GetStreams.Stream;
 using Discord;
-using Discord.Rest;
 using Discord.WebSocket;
 using Swarmer.Helpers;
 using Swarmer.Models;
@@ -11,11 +10,13 @@ namespace Swarmer.Services;
 
 public class DdStreamsPostingService : AbstractBackgroundService
 {
+	private readonly TimeSpan _maxBufferTime = TimeSpan.FromMinutes(10);
 	private readonly SocketTextChannel _ddPalsNotifChannel;
 	private readonly SocketTextChannel _ddInfoNotifChannel;
 	private readonly DatabaseService _dbContext;
 	private readonly TwitchAPI _api;
 	private readonly List<ActiveStream> _activeStreams;
+	private readonly List<BufferedStream> _streamBuffer = new();
 	private readonly StreamCache _streamCache;
 
 	public DdStreamsPostingService(
@@ -31,6 +32,9 @@ public class DdStreamsPostingService : AbstractBackgroundService
 		_streamCache = streamCache;
 
 		_activeStreams = _dbContext.ActiveDdStreams.ToList();
+		foreach (ActiveStream stream in _activeStreams)
+			_streamBuffer.Add(new(stream.StreamId, stream.DdpalsMessageId, stream.DdinfoMessageId, DateTime.UtcNow));
+
 		_ddPalsNotifChannel = client.GetChannel(ulong.Parse(Environment.GetEnvironmentVariable("DdPalsNotifChannel")!)) as SocketTextChannel ?? throw new ArgumentException("DdPalsNotifChannel");
 		_ddInfoNotifChannel = client.GetChannel(ulong.Parse(Environment.GetEnvironmentVariable("DdInfoNotifChannel")!)) as SocketTextChannel ?? throw new ArgumentException("DdInfoNotifChannel");
 	}
@@ -39,6 +43,8 @@ public class DdStreamsPostingService : AbstractBackgroundService
 
 	protected override async Task ExecuteTaskAsync(CancellationToken stoppingToken)
 	{
+		Console.WriteLine($"Stream buffer @{DateTime.Now.ToLongTimeString()}:\n{string.Join('\n', _streamBuffer)}");
+		CleanUpStreamBuffer();
 		await CheckTwitchStreams();
 	}
 
@@ -48,23 +54,16 @@ public class DdStreamsPostingService : AbstractBackgroundService
 		Stream[] twitchStreams = _streamCache.Cache;
 		foreach (Stream stream in twitchStreams)
 		{
-			if (_activeStreams.Exists(s => s.StreamId == stream.Id))
+			if (StreamStillActive(stream))
 				continue;
 
 			changed = true;
 			User twitchUser = (await _api.Helix.Users.GetUsersAsync(ids: new() { stream.UserId })).Users[0];
-			Embed streamEmbed = EmbedHelper.GetOnlineStreamEmbed(
-				stream.Title,
-				stream.UserName,
-				GetProperUrl(stream.ThumbnailUrl),
-				GetProperUrl(twitchUser.ProfileImageUrl),
-				"https://twitch.tv/" + stream.UserName);
+			(ulong ddpalsMessageId, ulong ddinfoMessageId) = await PostOrUpdateStreamMessage(stream, twitchUser);
+			ActiveStream newStream = new(stream.Id, stream.UserId, ddpalsMessageId, ddinfoMessageId, twitchUser.OfflineImageUrl);
 
-			RestUserMessage ddpalsMessage = await _ddPalsNotifChannel.SendMessageAsync(embed: streamEmbed);
-			RestUserMessage ddinfoMessage = await _ddInfoNotifChannel.SendMessageAsync(embed: streamEmbed);
-
-			ActiveStream newStream = new(stream.Id, stream.UserId, ddpalsMessage.Id, ddinfoMessage.Id, twitchUser.OfflineImageUrl);
 			_activeStreams.Add(newStream);
+			RefreshBufferedStreamOrCreateNew(stream.Id, ddpalsMessageId, ddinfoMessageId);
 			await _dbContext.ActiveDdStreams.AddAsync(newStream);
 		}
 
@@ -77,17 +76,69 @@ public class DdStreamsPostingService : AbstractBackgroundService
 
 			IUserMessage? ddpalsStreamMsg = await _ddPalsNotifChannel.GetMessageAsync(activeStream.DdpalsMessageId) as IUserMessage;
 			IUserMessage? ddinfoStreamMsg = await _ddInfoNotifChannel.GetMessageAsync(activeStream.DdinfoMessageId) as IUserMessage;
-
 			await MakeStreamEmbedOfflineIfPossible(ddpalsStreamMsg, activeStream.OfflineThumbnailUrl);
 			await MakeStreamEmbedOfflineIfPossible(ddinfoStreamMsg, activeStream.OfflineThumbnailUrl);
 
 			_activeStreams.Remove(activeStream);
+			RefreshBufferedStreamOrCreateNew(activeStream.StreamId, ddpalsStreamMsg?.Id, ddinfoStreamMsg?.Id);
 			_dbContext.ActiveDdStreams.Remove(activeStream);
 			changed = true;
 		}
 
 		if (changed)
 			await _dbContext.SaveChangesAsync();
+	}
+
+	private void RefreshBufferedStreamOrCreateNew(string streamId, ulong? ddpalsMessageId, ulong? ddinfoMessageId)
+	{
+		if (_streamBuffer.Find(bs => bs.StreamId == streamId) is { StreamId: { } } bufferedStream)
+			bufferedStream.DateAddedUtc = DateTime.UtcNow;
+		else
+			_streamBuffer.Add(new(streamId, ddpalsMessageId, ddinfoMessageId, DateTime.UtcNow));
+	}
+
+	private async Task<(ulong DdpalsMessageId, ulong DdinfoMessageId)> PostOrUpdateStreamMessage(Stream stream, User twitchUser)
+	{
+		Embed streamEmbed = EmbedHelper.GetOnlineStreamEmbed(
+			stream.Title,
+			stream.UserName,
+			GetProperUrl(stream.ThumbnailUrl),
+			GetProperUrl(twitchUser.ProfileImageUrl),
+			"https://twitch.tv/" + stream.UserName);
+
+		IUserMessage? ddpalsMsg = null;
+		IUserMessage? ddinfoMsg = null;
+		// Stream is active and in buffer
+		if (_streamBuffer.Find(bs => bs.StreamId == stream.Id) is { StreamId: not null } bufferedStream)
+		{
+			if (bufferedStream.DdpalsMessageId is not null)
+				ddpalsMsg = await _ddPalsNotifChannel.GetMessageAsync((ulong)bufferedStream.DdpalsMessageId) as IUserMessage;
+
+			if (bufferedStream.DdinfoMessageId is not null)
+				ddinfoMsg = await _ddInfoNotifChannel.GetMessageAsync((ulong)bufferedStream.DdinfoMessageId) as IUserMessage;
+
+			await MakeStreamEmbedOnlineIfPossible(ddpalsMsg, streamEmbed);
+			await MakeStreamEmbedOnlineIfPossible(ddinfoMsg, streamEmbed);
+		}
+
+		ddpalsMsg ??= await _ddPalsNotifChannel.SendMessageAsync(embed: streamEmbed);
+		ddinfoMsg ??= await _ddInfoNotifChannel.SendMessageAsync(embed: streamEmbed);
+
+		return (ddpalsMsg.Id, ddinfoMsg.Id);
+	}
+
+	private bool StreamStillActive(Stream stream) => _activeStreams.Exists(s => s.StreamId == stream.Id);
+
+	private void CleanUpStreamBuffer()
+	{
+		DateTime utcNow = DateTime.UtcNow;
+		_streamBuffer.RemoveAll(bs => utcNow - bs.DateAddedUtc >= _maxBufferTime);
+	}
+
+	private static async Task MakeStreamEmbedOnlineIfPossible(IUserMessage? streamMessage, Embed embed)
+	{
+		if (streamMessage is not null)
+			await streamMessage.ModifyAsync(m => m.Embed = embed);
 	}
 
 	private static async Task MakeStreamEmbedOfflineIfPossible(IUserMessage? streamMessage, string offlineThumbnailUrl)
@@ -99,6 +150,7 @@ public class DdStreamsPostingService : AbstractBackgroundService
 		}
 	}
 
-	private static string GetProperUrl(string url)
-		=> url.Replace("{height}", "1080").Replace("{width}", "1920");
+	private static string GetProperUrl(string url) => url.Replace("{height}", "1080").Replace("{width}", "1920");
+
+	private record struct BufferedStream(string StreamId, ulong? DdpalsMessageId, ulong? DdinfoMessageId, DateTime DateAddedUtc);
 }
