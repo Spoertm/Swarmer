f0ae0c8edd3081383a2e3202cc2ae7b6ffec08cd
Spoertm <spoertm@gmail.com>
2022-01-23 16:23:50 +0100

    Move to new dir

--- Models/Database/DdStream.cs
+++ Models/Database/DdStream.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Swarmer.Models;
+namespace Swarmer.Models.Database;
 
 public class DdStream
 {
--- Models/Database/DdStreamChannel.cs
+++ Models/Database/DdStreamChannel.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Swarmer.Models;
+namespace Swarmer.Models.Database;
 
 public class DdStreamChannel
 {
