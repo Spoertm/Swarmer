50387b83e90d1bcc81bf698768f3fb16ff05807a
Spoertm <spoertm@gmail.com>
2022-01-25 21:56:37 +0100

    Use FormatDimensions()

--- Utils/StreamEmbed.cs
+++ Utils/StreamEmbed.cs
@@ -1,4 +1,5 @@
 ﻿using Discord;
+using Swarmer.Models.Extensions;
 
 namespace Swarmer.Utils;
 
@@ -6,14 +7,14 @@ public static class StreamEmbed
 {
 	public static Embed Online(Stream stream, string? avatarUrl) => new EmbedBuilder()
 		.WithDescription("🔴 Live| " + stream.Title)
-		.WithThumbnailUrl(stream.ThumbnailUrl)
+		.WithThumbnailUrl(stream.ThumbnailUrl.FormatDimensions())
 		.WithAuthor(stream.UserName, avatarUrl, $"https://twitch.tv/{stream.UserName}")
 		.WithColor(6570404)
 		.Build();
 
 	public static Embed Offline(IEmbed oldEmbed, string? newThumbnailUrl = null) => new EmbedBuilder()
 		.WithDescription("⚫ Offline| " + (oldEmbed.Description?.Length >= 9 ? oldEmbed.Description[9..] : string.Empty))
-		.WithThumbnailUrl(newThumbnailUrl ?? oldEmbed.Thumbnail?.Url ?? string.Empty)
+		.WithThumbnailUrl(newThumbnailUrl?.FormatDimensions() ?? oldEmbed.Thumbnail?.Url ?? string.Empty)
 		.WithAuthor(oldEmbed.Author?.Name ?? "NaN", oldEmbed.Author?.IconUrl, oldEmbed.Author?.Url)
 		.WithColor(1)
 		.Build();
