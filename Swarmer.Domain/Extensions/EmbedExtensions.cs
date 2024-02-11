bf7fd568d24b6d8748ad07abd0fdc8ae1709f6c0
Spoertm <spoertm@gmail.com>
2024-02-11 11:38:45 +0100

    Revert "Sanitize embeds"
    This reverts commit 630ee5380199d1ad7ec66c50bbbc1cd5da2cb89e.

--- Swarmer.Domain/Extensions/EmbedExtensions.cs
+++ Swarmer.Domain/Extensions/EmbedExtensions.cs
@@ -5,14 +5,14 @@ namespace Swarmer.Domain.Extensions;
 public static class EmbedExtensions
 {
 	public static Embed Online(this EmbedBuilder builder, Stream stream, string? avatarUrl) => builder
-		.WithDescription("🔴 Live| " + Format.Sanitize(stream.Title))
+		.WithDescription("🔴 Live| " + stream.Title)
 		.WithThumbnailUrl(stream.ThumbnailUrl.FormatDimensions())
 		.WithAuthor(stream.UserName, avatarUrl, $"https://twitch.tv/{stream.UserLogin}")
 		.WithColor(6570404)
 		.Build();
 
 	public static Embed Offline(this EmbedBuilder builder, IEmbed oldEmbed, string? newThumbnailUrl = null) => builder
-		.WithDescription("⚫ Offline| " + Format.Sanitize(oldEmbed.Description?.Length >= 9 ? oldEmbed.Description[9..] : default))
+		.WithDescription("⚫ Offline| " + (oldEmbed.Description?.Length >= 9 ? oldEmbed.Description[9..] : default))
 		.WithThumbnailUrl(newThumbnailUrl?.FormatDimensions() ?? oldEmbed.Thumbnail?.Url ?? default)
 		.WithAuthor(oldEmbed.Author?.Name ?? "NaN", oldEmbed.Author?.IconUrl, oldEmbed.Author?.Url)
 		.WithColor(1)
