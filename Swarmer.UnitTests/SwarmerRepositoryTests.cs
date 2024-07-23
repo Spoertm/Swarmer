b4756091255c9cb81a644315fdb30a0680d394f0
Spoertm <spoertm@gmail.com>
2024-07-23 21:40:28 +0200

    Cleanup

--- Swarmer.UnitTests/SwarmerRepositoryTests.cs
+++ Swarmer.UnitTests/SwarmerRepositoryTests.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
-using Stream = TwitchLib.Api.Helix.Models.Streams.GetStreams.Stream;
 
 namespace Swarmer.UnitTests;
 
